287eb18584acc058896952dd88f6a1def4f8b617
benweet <benoit.schweblin@gmail.com>
2015-02-07 15:11:29 +0000

    Added app.classeur.io config

--- src/core/constants/constants.js
+++ src/core/constants/constants.js
@@ -1,8 +1,13 @@
 angular.module('classeur.core.constants', [])
-	.factory('clConstants', function() {
+	.factory('clConstants', function($location) {
 		var clConstants = {
 			googleClientId: '276184199763-tp3t95ddji4pfd6od4h0v14kaqvi8nb7.apps.googleusercontent.com',
 			serverUrl: 'http://localhost:11583'
 		};
+
+		if($location.host() === 'app.classeur.io') {
+			clConstants.serverUrl = 'http://app.classeur.io';
+		}
+		
 		return clConstants;
 	});
